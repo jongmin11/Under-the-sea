555e1e6ca96f1f7cf0bd693595540a332880d75a
jongmin11 <gkswhdals01@gmail.com>
2025-07-29 15:59:20 +0900

    스코어 스크립트생성
    스코어에대한 스크립트를 생성하엿음

---
+++ Assets/Script/ScoreManager.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 게임의 현재 점수 및 최고 점수를 관리하는 싱글톤 클래스입니다.
+/// </summary>
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance { get; private set; }
+    public int CurrentScore {  get; private set; } = 0;
+    public int HighScore { get; private set; } = 0;
+
+    const string HighScoreKey = "HighScore";
+
+    public void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            LoadHighScore();
+        }
+        else
+        {
+            Destroy(instance);
+        }
+    }
+
+    /// <summary>
+    /// 점수를 추가합니다. 최고 점수를 자동으로 갱신합니다.
+    /// </summary>
+    /// <param name="amount"></param>
+    public void AddScore(int amount)
+    {
+        CurrentScore += amount;
+
+        if (CurrentScore > HighScore)
+        {
+            HighScore = CurrentScore;
+            SaveHighScore();
+        }
+
+    }
+
+    /// <summary>
+    /// 현재 점수를 0으로 초기화합니다.  
+    /// 최고 점수는 그대로 유지됩니다.
+    /// </summary>
+    public void ResetScore()
+    {
+        CurrentScore = 0;
+    }
+
+    /// <summary>
+    /// 현재 최고 점수를 로컬 저장소(PlayerPrefs)에 저장합니다.
+    /// </summary>
+    void SaveHighScore()
+    {
+        PlayerPrefs.SetInt(HighScoreKey, HighScore);
+        PlayerPrefs.Save();
+    }
+
+
+    /// <summary>
+    /// 로컬 저장소(PlayerPrefs)에서 최고 점수를 불러옵니다.
+    /// 값이 없을 경우 0으로 초기화됩니다.
+    /// </summary>
+    void LoadHighScore()
+    {
+        HighScore = PlayerPrefs.GetInt(HighScoreKey);
+    }
+
+}
