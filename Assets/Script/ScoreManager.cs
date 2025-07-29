369b67f8c8a46502c121c0038e0eb5f9538de5f9
jongmin11 <gkswhdals01@gmail.com>
2025-07-29 19:02:25 +0900

    테스트

--- Assets/Script/ScoreManager.cs
+++ Assets/Script/ScoreManager.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 using UnityEngine.UI;
 
 /// <summary>
-/// 게임의 현재 점수 및 최고 점수를 관리하는 싱글톤 클래스입니다.
+/// (한종민) 게임의 현재 점수 및 최고 점수를 관리하는 싱글톤 클래스입니다.
 /// </summary>
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager instance { get; private set; }
-    public int CurrentScore {  get; private set; } = 0;
+    public int CurrentScore { get; private set; } = 0;
+    
     public int HighScore { get; private set; } = 0;
 
     const string HighScoreKey = "HighScore";
@@ -29,8 +30,9 @@ public class ScoreManager : MonoBehaviour
         }
     }
 
+
     /// <summary>
-    /// 점수를 추가합니다. 최고 점수를 자동으로 갱신합니다.
+    /// (한종민) 점수를 추가합니다. 최고 점수를 자동으로 갱신합니다.
     /// </summary>
     /// <param name="amount"></param>
     public void AddScore(int amount)
@@ -42,11 +44,10 @@ public class ScoreManager : MonoBehaviour
             HighScore = CurrentScore;
             SaveHighScore();
         }
-
     }
 
     /// <summary>
-    /// 현재 점수를 0으로 초기화합니다.  
+    /// (한종민) 현재 점수를 0으로 초기화합니다.  
     /// 최고 점수는 그대로 유지됩니다.
     /// </summary>
     public void ResetScore()
@@ -55,7 +56,7 @@ public class ScoreManager : MonoBehaviour
     }
 
     /// <summary>
-    /// 현재 최고 점수를 로컬 저장소(PlayerPrefs)에 저장합니다.
+    /// (한종민) 현재 최고 점수를 로컬 저장소(PlayerPrefs)에 저장합니다.
     /// </summary>
     void SaveHighScore()
     {
@@ -63,14 +64,12 @@ public class ScoreManager : MonoBehaviour
         PlayerPrefs.Save();
     }
 
-
     /// <summary>
-    /// 로컬 저장소(PlayerPrefs)에서 최고 점수를 불러옵니다.
+    /// (한종민) 로컬 저장소(PlayerPrefs)에서 최고 점수를 불러옵니다.
     /// 값이 없을 경우 0으로 초기화됩니다.
     /// </summary>
     void LoadHighScore()
     {
         HighScore = PlayerPrefs.GetInt(HighScoreKey);
     }
-
 }
