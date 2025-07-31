0a75dc07aaab35c66fbda4375a9a48e0a191984a
JiWon1211 <ken0993@naver.com>
2025-07-31 12:14:58 +0900

    [ADD] 타이틀 배경음 버튼 효과음 추가

--- Assets/Script/UI/TitleUIManager.cs
+++ Assets/Script/UI/TitleUIManager.cs
@@ -5,8 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class TitleUIManager : MonoBehaviour
 {
+    public AudioSource sfxPlayer;           // 버튼 효과음 재생기
+    public AudioClip clickSound;            // 버튼 클릭 소리
+    public string sceneToLoad = "MainGameScene"; // 로드할 씬 이름
+
     public void StartGame()
     {
-        SceneManager.LoadScene("MainGameScene"); // 게임시작
+        // 효과음 재생
+        if (sfxPlayer && clickSound)
+        {
+            sfxPlayer.PlayOneShot(clickSound);
+        }
+
+        
+        Invoke("LoadNextScene", 0.3f);
+    }
+
+    void LoadNextScene()
+    {
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
+
